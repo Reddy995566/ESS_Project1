4bb3bbc19ef45b1ce8378f7fbb99e515da0e8569
KAJAL DAS <733kajaldas@gmail.com>
2026-01-18 16:41:32 +0530

    ok

--- app/Http/Controllers/ManifestController.php
+++ app/Http/Controllers/ManifestController.php
@@ -13,10 +13,6 @@ class ManifestController extends Controller
         $siteDescription = Setting::get('site_description', 'Premium Fashion Store - Shop Latest Trends');
         $siteLogo = Setting::get('site_logo', '');
         
-        // Generate icon URLs from logo
-        $icon192 = $siteLogo ?: asset('pwa-icon-192.png');
-        $icon512 = $siteLogo ?: asset('pwa-icon-512.png');
-        
         $manifest = [
             'name' => $siteName,
             'short_name' => substr($siteName, 0, 12),
@@ -26,21 +22,25 @@ class ManifestController extends Controller
             'background_color' => '#FAF5ED',
             'theme_color' => '#4b0f27',
             'orientation' => 'portrait-primary',
-            'icons' => [
+        ];
+        
+        // Only add icons if logo exists
+        if ($siteLogo) {
+            $manifest['icons'] = [
                 [
-                    'src' => $icon192,
+                    'src' => $siteLogo,
                     'sizes' => '192x192',
                     'type' => 'image/png',
                     'purpose' => 'any maskable'
                 ],
                 [
-                    'src' => $icon512,
+                    'src' => $siteLogo,
                     'sizes' => '512x512',
                     'type' => 'image/png',
                     'purpose' => 'any maskable'
                 ]
-            ]
-        ];
+            ];
+        }
         
         return response()->json($manifest)
             ->header('Content-Type', 'application/manifest+json');
