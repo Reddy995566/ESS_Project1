54d89620e46683fc14f298225a6519babaa7304b
KAJAL DAS <733kajaldas@gmail.com>
2026-01-18 16:46:49 +0530

    ok

--- app/Http/Controllers/ManifestController.php
+++ app/Http/Controllers/ManifestController.php
@@ -11,9 +11,10 @@ class ManifestController extends Controller
     {
         $siteName = Setting::get('site_name', 'Fashion Store');
         $siteDescription = Setting::get('site_description', 'Premium Fashion Store - Shop Latest Trends');
+        $siteLogo = Setting::get('site_logo', '');
         
-        // Use the uploaded icon
-        $iconUrl = asset('website/1768647579_696b6b9b91f3a.png');
+        // Use database logo if available, otherwise fallback to uploaded icon
+        $iconUrl = $siteLogo ?: asset('website/1768647579_696b6b9b91f3a.png');
         
         $manifest = [
             'name' => $siteName,
